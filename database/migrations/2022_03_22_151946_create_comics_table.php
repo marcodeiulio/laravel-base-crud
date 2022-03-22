abb590f2892822c2bdaa77b633302279bd388319
marcodeiulio <mdeiulio@icloud.com>
2022-03-22 16:55:23 +0100

    - Added Seeder - Prepared & made seeding - Edited migration (attributes' length)

--- database/migrations/2022_03_22_151946_create_comics_table.php
+++ database/migrations/2022_03_22_151946_create_comics_table.php
@@ -15,13 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50);
+            $table->string('title', 100);
             $table->text('description')->nullable();
             $table->string('thumb')->nullable();
             $table->float('price', 5, 2)->unsigned();
-            $table->string('series', 30);
+            $table->string('series', 50);
             $table->date('sale_date');
-            $table->string('type', 25);
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
